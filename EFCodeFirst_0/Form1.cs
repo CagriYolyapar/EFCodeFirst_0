e1ff72144511b098fb7c5dc5b9516c72e8108d04
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-12-09 13:40:19 +0300

    Singleton Pattern eklendi

--- EFCodeFirst_0/Form1.cs
+++ EFCodeFirst_0/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EFCodeFirst_0.DesignPatterns.SingletonPattern;
+using EFCodeFirst_0.Models.ContextClasses;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,16 @@ namespace EFCodeFirst_0
 {
     public partial class Form1 : Form
     {
+        MyContext _db;
         public Form1()
         {
             InitializeComponent();
+            _db = DBTool.DbInstance;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = _db.Categories.ToList();
         }
     }
 }
